eb30fc9d49a9eda496a6162ba90b485dcc32fe7a
Zuyyin <zuyyinzafirah89@gmail.com>
2025-08-17 23:30:55 +0700

    Commit Pertama

---
+++ app/Http/Controllers/registerController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class registerController extends Controller
+{
+    public function index()
+    {
+        return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email:dns|unique:users',
+            'password' => 'required|string|min:5'
+        ]);
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+        return to_route('login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\dashboardController;
+use App\Http\Controllers\loginController;
+use App\Http\Controllers\registerController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('kerangka.master');
+//});
+
+Route::get('/dashboard', [dashboardController::class, 'index']);
+
+Route::post('/logout', [loginController::class, 'logout'])->name('logout');
+
+Route::get('/', [loginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/log', [loginController::class, 'login'])->name('login.store');
+
+Route::get('/register', [registerController::class, 'index'])->name('register');
+Route::post('/regist', [registerController::class, 'store'])->name('register.store');

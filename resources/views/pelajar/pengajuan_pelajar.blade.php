d37f12dcede074834625914f838a9f3b97f5083d
Teguhalafanta <teguh.alafanta02@gmail.com>
2025-09-11 16:14:06 +0700

    perbaikan error pengisian form

--- resources/views/pelajar/pengajuan_pelajar.blade.php
+++ resources/views/pelajar/pengajuan_pelajar.blade.php
@@ -13,7 +13,7 @@
                     {{-- Nama --}}
                     <div class="mb-3">
                         <label class="form-label">Nama Lengkap</label>
-                        <input type="text" name="nama" class="form-control" required>
+                        <input type="text" name="nama" class="form-control" value="{{ old('nama') }}" required>
                     </div>
 
                     {{-- Jenis Kelamin --}}
@@ -21,33 +21,37 @@
                         <label class="form-label">Jenis Kelamin</label>
                         <select name="jenis_kelamin" class="form-control" required>
                             <option value="">-- Pilih --</option>
-                            <option value="Laki-laki">Laki-laki</option>
-                            <option value="Perempuan">Perempuan</option>
+                            <option value="Laki-laki" {{ old('jenis_kelamin') == 'Laki-laki' ? 'selected' : '' }}>Laki-laki
+                            </option>
+                            <option value="Perempuan" {{ old('jenis_kelamin') == 'Perempuan' ? 'selected' : '' }}>Perempuan
+                            </option>
                         </select>
                     </div>
 
                     {{-- Tempat Lahir --}}
                     <div class="mb-3">
                         <label class="form-label">Tempat Lahir</label>
-                        <input type="text" name="tempat_lahir" class="form-control" required>
+                        <input type="text" name="tempat_lahir" class="form-control" value="{{ old('tempat_lahir') }}"
+                            required>
                     </div>
 
                     {{-- Tanggal Lahir --}}
                     <div class="mb-3">
                         <label class="form-label">Tanggal Lahir</label>
-                        <input type="date" name="tanggal_lahir" class="form-control" required>
+                        <input type="date" name="tanggal_lahir" class="form-control" value="{{ old('tanggal_lahir') }}"
+                            required>
                     </div>
 
                     {{-- Alamat --}}
                     <div class="mb-3">
                         <label class="form-label">Alamat</label>
-                        <textarea name="alamat" class="form-control" rows="3" required></textarea>
+                        <textarea name="alamat" class="form-control" rows="3" required>{{ old('alamat') }}</textarea>
                     </div>
 
                     {{-- Telepon --}}
                     <div class="mb-3">
                         <label class="form-label">No. Telepon</label>
-                        <input type="text" name="telepon" class="form-control">
+                        <input type="text" name="telepon" class="form-control" value="{{ old('telepon') }}">
                     </div>
                 </div>
 
@@ -56,43 +60,46 @@
                     {{-- Email --}}
                     <div class="mb-3">
                         <label class="form-label">Email</label>
-                        <input type="email" name="email" class="form-control" required>
+                        <input type="email" name="email" class="form-control" value="{{ old('email') }}" required>
                     </div>
 
                     {{-- NIM / NISN --}}
                     <div class="mb-3">
                         <label class="form-label">NIM / NISN</label>
-                        <input type="text" name="nim_nisn" class="form-control" required>
+                        <input type="text" name="nim_nisn" class="form-control" value="{{ old('nim_nisn') }}" required>
                     </div>
 
                     {{-- Asal Institusi --}}
                     <div class="mb-3">
                         <label class="form-label">Asal Institusi</label>
-                        <input type="text" name="asal_institusi" class="form-control" required>
+                        <input type="text" name="asal_institusi" class="form-control"
+                            value="{{ old('asal_institusi') }}" required>
                     </div>
 
                     {{-- Fakultas --}}
                     <div class="mb-3">
                         <label class="form-label">Fakultas</label>
-                        <input type="text" name="fakultas" class="form-control">
+                        <input type="text" name="fakultas" class="form-control" value="{{ old('fakultas') }}">
                     </div>
 
                     {{-- Jurusan --}}
                     <div class="mb-3">
                         <label class="form-label">Jurusan</label>
-                        <input type="text" name="jurusan" class="form-control" required>
+                        <input type="text" name="jurusan" class="form-control" value="{{ old('jurusan') }}" required>
                     </div>
 
                     {{-- Rencana Mulai --}}
                     <div class="mb-3">
                         <label class="form-label">Rencana Mulai</label>
-                        <input type="date" name="rencana_mulai" class="form-control" required>
+                        <input type="date" name="rencana_mulai" class="form-control" value="{{ old('rencana_mulai') }}"
+                            required>
                     </div>
 
                     {{-- Rencana Selesai --}}
                     <div class="mb-3">
                         <label class="form-label">Rencana Selesai</label>
-                        <input type="date" name="rencana_selesai" class="form-control" required>
+                        <input type="date" name="rencana_selesai" class="form-control"
+                            value="{{ old('rencana_selesai') }}" required>
                     </div>
 
                     {{-- Upload Proposal --}}

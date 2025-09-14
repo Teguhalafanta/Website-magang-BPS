549be40a384602f6f9164f8b978eee7c0d29c03e
zahlul <zahlul.2001@gmail.com>
2025-09-14 22:37:00 +0700

    merapikan controller login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
-class LoginController extends Controller
+class AuthController extends Controller
 {
     public function index()
     {
@@ -29,16 +31,41 @@ class LoginController extends Controller
             return redirect()->route('pelajar.dashboard');
         }
 
-
         return back()->with('loginError', 'Login Failed');
+    }
+
+    public function sso()
+    {
+        return view('auth.login_sso');
+    }
+
+    public function register()
+    {
+        return view('auth.register_pelajar');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'username' => 'required|string|max:255|unique:users,username',
+            'email'    => 'required|string|email|max:255|unique:users,email',
+            'password' => 'required|string|min:5',
+        ]);
+
+        // Simpan ke tabel users
+        User::create([
+            'username' => $request->username,
+            'email'    => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect()->route('login')->with('success', 'Registrasi berhasil, silakan login.');
     }
 
     public function logout(Request $request)
     {
         Auth::logout();
-
         $request->session()->invalidate();
-
         $request->session()->regenerateToken();
 
         return redirect('/');

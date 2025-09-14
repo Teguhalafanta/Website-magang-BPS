0e30c73491caa7c98488f8e871f7228ae0380312
zahlul <zahlul.2001@gmail.com>
2025-09-14 22:58:36 +0700

    pindahkan folder include

--- resources/views/kerangka/include/footer.blade.php
+++ resources/views/kerangka/include/footer.blade.php
@@ -3,7 +3,7 @@
 <footer>
     <div class="footer clearfix mb-0 text-muted">
         <div class="float-start">
-            <p>2025 &copy; BPS Provinsi  Aceh</p>
+            <p>2025 &copy; BPS Provinsi Aceh</p>
         </div>
         <div class="float-end">
             <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a

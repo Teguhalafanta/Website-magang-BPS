27f01d44939f560e6cfb802c53e785437aa96148
Zahlul <zahlul.2001@gmail.com>
2025-10-14 15:05:05 +0700

    perbaiki migrations bug

--- database/migrations/2025_10_14_080350_add_pembimbing_id_to_pelajars_table.php
+++ database/migrations/2025_10_14_080350_add_pembimbing_id_to_pelajars_table.php
@@ -4,20 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::table('pelajars', function (Blueprint $table) {
-            $table->unsignedBigInteger('pembimbing_id')->nullable()->after('user_id');
-            $table->foreign('pembimbing_id')->references('id')->on('pembimbings')->onDelete('set null');
+            //
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::table('pelajars', function (Blueprint $table) {
-            $table->dropForeign(['pembimbing_id']);
-            $table->dropColumn('pembimbing_id');
+            //
         });
     }
 };

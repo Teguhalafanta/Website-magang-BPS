fe012ce1f10350f0f2f0e8c154b71b8a5d06124b
Zahlul <zahlul.2001@gmail.com>
2025-10-16 09:18:48 +0700

    bug migrations pembimbing_id

--- database/migrations/2025_10_14_080350_add_pembimbing_id_to_pelajars_table.php
+++ database/migrations/2025_10_14_080350_add_pembimbing_id_to_pelajars_table.php
@@ -12,17 +12,16 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('pelajars', function (Blueprint $table) {
-            //
+            $table->unsignedBigInteger('pembimbing_id')->nullable()->after('user_id');
+            $table->foreign('pembimbing_id')->references('id')->on('pembimbings')->onDelete('set null');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('pelajars', function (Blueprint $table) {
-            //
+            $table->dropForeign(['pembimbing_id']);
+            $table->dropColumn('pembimbing_id');
         });
     }
 };
